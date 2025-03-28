2503c5e93fd3a305a765ef8f32f9968d73e7230f
Bhavan23 <bhavansathru.it@gmail.com>
2025-03-28 17:43:46 +0530

    feat: pretty-print status logs

--- india_banking_h2h/india_banking_h2h/doctype/status_log/status_log.js
+++ india_banking_h2h/india_banking_h2h/doctype/status_log/status_log.js
@@ -1,8 +1,77 @@
 // Copyright (c) 2025, Aerele Technologies Private Limited and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Status Log", {
-// 	refresh(frm) {
+frappe.ui.form.on("Status Log", {
+  pretty_format_json(frm) {
+    if (!frm.is_dirty() && !!frm.doc.response) {
+      let parsedData = JSON.parse(frm.doc.response);
+      let responses = Object.entries(parsedData).map(([key, value]) => ({
+        key,
+        ...value,
+      }));
+      showStatusList(responses);
+    }
+  },
+});
 
-// 	},
-// });
+function showStatusList(statuss) {
+  let list_html = `<ul class="list-group">`;
+  statuss.forEach((txn, index) => {
+    list_html += `
+            <li class="list-group-item">
+                <button class="btn btn-link" onclick="showStatusDetails(${index})">
+                    <b>Status</b>(${txn.key}) - ₹${txn.amount}
+                </button>
+            </li>`;
+  });
+  list_html += `</ul>`;
+
+  let listDialog = new frappe.ui.Dialog({
+    title: "Statuss List",
+    fields: [
+      {
+        fieldname: "status_list",
+        fieldtype: "HTML",
+        options: list_html,
+      },
+    ],
+    size: "large",
+  });
+
+  listDialog.show();
+
+  window.showStatusDetails = function (index) {
+    showStatusDetails(statuss[index]);
+  };
+}
+
+/**
+ * Show status details in a Frappe Dialog.
+ */
+function showStatusDetails(status) {
+  let details_html = `<table class="table table-bordered" style="width: 100%;">
+        ${Object.entries(status)
+          .map(
+            ([key, value]) => `
+            <tr>
+                <td><b>${key}</b></td>
+                <td>${value || "-"}</td>
+            </tr>`
+          )
+          .join("")}
+    </table>`;
+
+  let detailsDialog = new frappe.ui.Dialog({
+    title: `Status: ${status.key}`,
+    fields: [
+      {
+        fieldname: "status_data",
+        fieldtype: "HTML",
+        options: details_html,
+      },
+    ],
+    size: "large",
+  });
+
+  detailsDialog.show();
+}
