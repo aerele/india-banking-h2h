7de0cbc8e3b58d5f1495cc0f39913634e2f400fa
Bhavan23 <bhavansathru.it@gmail.com>
2025-07-03 12:04:20 +0530

    feat(payment): create and upload payment file based on mode of transfer feat(status): retrieve status file via SFTP, store locally, and process payment status updates

--- india_banking_h2h/india_banking_h2h/doctype/status_log/status_log.js
+++ india_banking_h2h/india_banking_h2h/doctype/status_log/status_log.js
@@ -2,76 +2,88 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Status Log", {
-  pretty_format_json(frm) {
-    if (!frm.is_dirty() && !!frm.doc.response) {
-      let parsedData = JSON.parse(frm.doc.response);
-      let responses = Object.entries(parsedData).map(([key, value]) => ({
-        key,
-        ...value,
-      }));
-      showStatusList(responses);
-    }
-  },
+	refresh(frm) {
+		frm.add_custom_button("Decrypt File", function () {
+			frm.call("decrypt_file").then((r) => {
+				frm.reload_doc();
+			});
+		});
+		frm.add_custom_button("Get Formatted Response", function () {
+			frm.call("format_response").then((r) => {
+				frm.reload_doc();
+			});
+		});
+	},
+	pretty_format_json(frm) {
+		if (!frm.is_dirty() && !!frm.doc.response) {
+			let parsedData = JSON.parse(frm.doc.response);
+			let responses = Object.entries(parsedData).map(([key, value]) => ({
+				key,
+				...value,
+			}));
+			showStatusList(responses);
+		}
+	},
 });
 
 function showStatusList(statuss) {
-  let list_html = `<ul class="list-group">`;
-  statuss.forEach((txn, index) => {
-    list_html += `
+	let list_html = `<ul class="list-group">`;
+	statuss.forEach((txn, index) => {
+		list_html += `
             <li class="list-group-item">
                 <button class="btn btn-link" onclick="showStatusDetails(${index})">
                     <b>Status</b>(${txn.key}) - ₹${txn.amount}
                 </button>
             </li>`;
-  });
-  list_html += `</ul>`;
+	});
+	list_html += `</ul>`;
 
-  let listDialog = new frappe.ui.Dialog({
-    title: "Statuss List",
-    fields: [
-      {
-        fieldname: "status_list",
-        fieldtype: "HTML",
-        options: list_html,
-      },
-    ],
-    size: "large",
-  });
+	let listDialog = new frappe.ui.Dialog({
+		title: "Statuss List",
+		fields: [
+			{
+				fieldname: "status_list",
+				fieldtype: "HTML",
+				options: list_html,
+			},
+		],
+		size: "large",
+	});
 
-  listDialog.show();
+	listDialog.show();
 
-  window.showStatusDetails = function (index) {
-    showStatusDetails(statuss[index]);
-  };
+	window.showStatusDetails = function (index) {
+		showStatusDetails(statuss[index]);
+	};
 }
 
 /**
  * Show status details in a Frappe Dialog.
  */
 function showStatusDetails(status) {
-  let details_html = `<table class="table table-bordered" style="width: 100%;">
+	let details_html = `<table class="table table-bordered" style="width: 100%;">
         ${Object.entries(status)
-          .map(
-            ([key, value]) => `
+			.map(
+				([key, value]) => `
             <tr>
                 <td><b>${key}</b></td>
                 <td>${value || "-"}</td>
             </tr>`
-          )
-          .join("")}
+			)
+			.join("")}
     </table>`;
 
-  let detailsDialog = new frappe.ui.Dialog({
-    title: `Status: ${status.key}`,
-    fields: [
-      {
-        fieldname: "status_data",
-        fieldtype: "HTML",
-        options: details_html,
-      },
-    ],
-    size: "large",
-  });
+	let detailsDialog = new frappe.ui.Dialog({
+		title: `Status: ${status.key}`,
+		fields: [
+			{
+				fieldname: "status_data",
+				fieldtype: "HTML",
+				options: details_html,
+			},
+		],
+		size: "large",
+	});
 
-  detailsDialog.show();
+	detailsDialog.show();
 }
